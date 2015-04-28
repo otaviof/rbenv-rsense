0142ef6779611258cbb9350e60421d5c0c0a0e91
Otavio Fernandes <ofernandes@schubergphilis.com>
2015-04-28 11:30:35 +0200

    Adding rehash hook.

---
+++ etc/rbenv.d/rehash/rsense-config.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# Summary: Reconfigure RSense to follow up rbenv, during 'rehash' hook.
+
+set -e
+[ -n "$RBENV_DEBUG" ] && set -x
+[ -z "$RBENV_PLUGIN_NAME" ] && RBENV_PLUGIN_NAME="rsense"
+
+RBENV_PLUGIN_ROOT="${RBENV_ROOT}/plugins/rbenv-${RBENV_PLUGIN_NAME}"
+RBENV_PLUGIN_SOURCE=1
+
+. "${RBENV_PLUGIN_ROOT}/bin/rbenv-${RBENV_PLUGIN_NAME}"
+. "${RBENV_PLUGIN_ROOT}/bin/rbenv-${RBENV_PLUGIN_NAME}-config"
+
+check_rsense_home "config"
+
+# EOF
